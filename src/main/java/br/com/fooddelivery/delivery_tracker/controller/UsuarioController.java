fcb456c5046fd4470d3102716a360ba3bc9c1b36
leticiafdepaula <leticiaferreira150998@gmail.com>
2026-07-31 01:19:10 -0300

    feat: ajuste nas dependencias do pom

--- src/main/java/br/com/fooddelivery/delivery_tracker/controller/UsuarioController.java
+++ src/main/java/br/com/fooddelivery/delivery_tracker/controller/UsuarioController.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UsuarioController {
 
-
     private final UsuarioService usuarioService;
-
 
     @PostMapping
     public ResponseEntity<UsuarioResponse> cadastrar(
@@ -31,5 +29,4 @@ public class UsuarioController {
                 .status(HttpStatus.CREATED)
                 .body(response);
     }
-
 }
